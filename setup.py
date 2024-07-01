6f39b657bb7f844b64419ff3cbe38955aea06afb
nandhinikesavan20 <113448447+nandhinikesavan20@users.noreply.github.com>
2024-07-01 16:30:50 +0530

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,25 @@
+# from setuptools import setup, find_packages
+
+# VERSION = '0.0.1'
+# DESCRIPTION = 'A basic hello package'
+
+# # Setting up
+# setup(
+#     name="hello_world_nk",
+#     version=VERSION,
+#     author="Nandhini",
+#     author_email="<nandhini67288@gmail.com>",
+#     description=DESCRIPTION,
+#     long_description_content_type="text/markdown",
+#     packages=find_packages(),
+#     install_requires=['opencv-python'],
+#     keywords=['python','hello'],
+#     classifiers=[
+#         "Development Status :: 1 - Planning",
+#         "Intended Audience :: Developers",
+#         "Programming Language :: Python :: 3",
+#         "Operating System :: Unix",
+#         "Operating System :: MacOS :: MacOS X",
+#         "Operating System :: Microsoft :: Windows",
+#     ]
+# )
